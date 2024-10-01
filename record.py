340ca6037f5970553524745ae221b5e6b6b641a1
STK-28 <son_tk28@hotmail.com>
2024-10-01 14:52:09 +0700

    it nice

--- record.py
+++ record.py
@@ -1,34 +1,32 @@
 
 """
-record.py
+record2.py
 ---------------
 
 Main Function for recording a video sequence into cad (color-aligned-to-depth) 
 images and depth images
 
-This code is compatible with legacy camera models supported on librealsense SDK v1
-and use 3rd party python wrapper https://github.com/toinsson/pyrealsense
-
-For the newer D series cameras, please use record2.py
+Using librealsense SDK 2.0 with pyrealsense2 for SR300 and D series cameras
 
 
 """
 
-# record for 30s after a 5s count down
+# record for 40s after a 5s count down
 # or exit the recording earlier by pressing q
 
-RECORD_LENGTH = 30
+RECORD_LENGTH = 40
 
 import png
+import pyrealsense2 as rs
 import json
 import logging
 logging.basicConfig(level=logging.INFO)
 import numpy as np
 import cv2
-import pyrealsense2 as rs
 import time
 import os
 import sys
+# from config.DataAcquisitionParameters import DEPTH_THRESH
 
 def make_directories(folder):
     if not os.path.exists(folder+"JPEGImages/"):
@@ -38,9 +36,9 @@ def make_directories(folder):
 
 def print_usage():
     
-    print("Usage: record.py <foldername>")
+    print("Usage: record2.py <foldername>")
     print("foldername: path where the recorded data should be stored at")
-    print("e.g., record.py LINEMOD/mug")
+    print("e.g., record2.py LINEMOD/mug")     
     
 
 if __name__ == "__main__":
@@ -50,79 +48,78 @@ if __name__ == "__main__":
         print_usage()
         exit()
 
+    FileName=0
     make_directories(folder)
+    
     pipeline = rs.pipeline()
     config = rs.config()
-
-    #config frame
-    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
     config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-
-    #Start record
-    profile = pipeline.start(config)
-
-    depth_sensor = profile.get_device().first_depth_sensor()
-    depth_scale = depth_sensor.get_depth_scale()
-
-    intr = profile.get_stream(rs.stream.color).as_video_stream_profile().get_intrinsics()
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
     
+    # Start pipeline
+    profile = pipeline.start(config)
+    frames = pipeline.wait_for_frames()
+    color_frame = frames.get_color_frame()
 
+    # Color Intrinsics 
+    intr = color_frame.profile.as_video_stream_profile().intrinsics
     camera_parameters = {'fx': intr.fx, 'fy': intr.fy,
-                        'ppx': intr.ppx, 'ppy': intr.ppy,
-                        'height': intr.height, 'width': intr.width,
-                        'depth_scale': depth_scale}
+                         'ppx': intr.ppx, 'ppy': intr.ppy,
+                         'height': intr.height, 'width': intr.width,
+                         'depth_scale':profile.get_device().first_depth_sensor().get_depth_scale()
+    }
+
     
     with open(folder+'intrinsics.json', 'w') as fp:
         json.dump(camera_parameters, fp)
 
-        FileName = 0
-        T_start = time.time()
+    align_to = rs.stream.color
+    align = rs.align(align_to)
 
-    try:
-        while True:
-            frames = pipeline.wait_for_frames()
-            color_frame = frames.get_color_frame()
-            depth_frame = frames.get_depth_frame()
+    T_start = time.time()
+    while True:
+        frames = pipeline.wait_for_frames()
+        aligned_frames = align.process(frames)
 
-            if not color_frame or not depth_frame:
-                continue
+        aligned_depth_frame = aligned_frames.get_depth_frame()
+        color_frame = aligned_frames.get_color_frame()
 
-            # Convert images to numpy arrays
-            color_image = np.asanyarray(color_frame.get_data())
-            depth_image = np.asanyarray(depth_frame.get_data())
+        # Validate that both frames are valid
+        if not aligned_depth_frame or not color_frame:
+            continue
 
-            # Visualize countdown
-            if time.time() - T_start > 5:
-                filecad = folder + "JPEGImages/%s.jpg" % FileName
-                filedepth = folder + "depth/%s.png" % FileName
-                cv2.imwrite(filecad, color_image)
+        d = np.asanyarray(aligned_depth_frame.get_data())
+        c = np.asanyarray(color_frame.get_data())
+   
+        # Visualize count down
 
-                # Save depth image
-                with open(filedepth, 'wb') as f:
-                    writer = png.Writer(width=depth_image.shape[1], height=depth_image.shape[0], bitdepth=16, greyscale=True)
-                    depth_list = depth_image.tolist()
-                    writer.write(f, depth_list)
+        if time.time() -T_start > 5:
+            filecad= folder+"JPEGImages/%s.jpg" % FileName
+            filedepth= folder+"depth/%s.png" % FileName
+            cv2.imwrite(filecad,c)
+            with open(filedepth, 'wb') as f:
+                writer = png.Writer(width=d.shape[1], height=d.shape[0],
+                                    bitdepth=16, greyscale=True)
+                zgray2list = d.tolist()
+                writer.write(f, zgray2list)
 
-                FileName += 1
+            FileName+=1
+        if time.time() -T_start > RECORD_LENGTH + 5:
+            pipeline.stop()
+            break
 
-            if time.time() - T_start > RECORD_LENGTH + 5:
-                break
+        if time.time() -T_start < 5:
+            cv2.putText(c,str(5-int(time.time() -T_start)),(240,320), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 4,(0,0,255),2,cv2.LINE_AA)
+        if time.time() -T_start > RECORD_LENGTH:
+            cv2.putText(c,str(RECORD_LENGTH+5-int(time.time()-T_start)),(240,320), cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 4,(0,0,255),2,cv2.LINE_AA)
+        cv2.imshow('COLOR IMAGE',c)
 
-            if time.time() - T_start < 5:
-                cv2.putText(color_image, str(5 - int(time.time() - T_start)), (240, 320), cv2.FONT_HERSHEY_SIMPLEX, 4, (0, 0, 255), 2, cv2.LINE_AA)
-            if time.time() - T_start > RECORD_LENGTH:
-                cv2.putText(color_image, str(RECORD_LENGTH + 5 - int(time.time() - T_start)), (240, 320), cv2.FONT_HERSHEY_SIMPLEX, 4, (0, 0, 255), 2, cv2.LINE_AA)
 
-            # Display the resulting frame
-            cv2.imshow('COLOR FRAME', color_image)
 
-            # Press 'q' to stop
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-
-    finally:
-        # Stop streaming
-        pipeline.stop()
+        # press q to quit the program
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            pipeline.stop()
+            break
 
     # Release everything if job is finished
     cv2.destroyAllWindows()
